0a87fe91d2e38c79589bc126a493256abb7478b2
wilffren <wilffren14@gmail.com>
2025-11-25 14:18:30 -0500

    [PRE-1] Initial setup: Spring Boot, Hexagonal Arch and Docker config

---
+++ src/test/java/com/predictifylabs/backend/PredictifylabsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.predictifylabs.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictifylabsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
